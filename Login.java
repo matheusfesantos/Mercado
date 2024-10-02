02eb6da86032ee66ca6e59aef8da639a06174268
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-10-02 16:30:02 -0300

    v0.1

--- Login.java
+++ Login.java
@@ -22,6 +22,6 @@ public class Login{
             } else {
                 System.out.println("Login ou senha incorretos. Tente novamente.");
                 Login.main();
-            }
+            }/
         }
     }
